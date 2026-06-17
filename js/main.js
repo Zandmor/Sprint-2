a905798db8308d3aed14d12a7987b82c1d2a6aac
Atir <66091842+Zandmor@users.noreply.github.com>
2026-06-17 14:24:21 +0300

    text generator works. buttons cleaned up, barefoot download section added

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ function onInit() {
     gCanvas.height = 400
     gCtx = gCanvas.getContext("2d")
     gCurrentTag = gMemes
+    searchInput()
 }
-
 function scaleCanvas(img) {
     const maxHeight = 400
 
@@ -25,7 +25,61 @@ function scaleCanvas(img) {
         gCanvas.height = maxHeight
         gCanvas.width = gCanvas.width * ratio
     }
+}
+function searchInput() {
+    var input = document.querySelector(".searchTag")
 
+    input.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+
+            onSearch()
+        }
+    })
+}
+
+function onSearch(){
+const input = document.querySelector(".searchTag")
+    const tag = input.value
+    onTag(tag)
+
+}
+
+function onFunny(){
+    onTag("funny")
+}
+function onCrying(){
+    onTag("crying")
+}
+function onLaughing(){
+    onTag("laughing")
+}
+function onAnimal(){
+    onTag("animal")
+}
+
+
+
+function onTag(tag) {
+    
+    const currImgs = document.querySelector(".img-gallery")
+    var newImgs = ""
+
+    gCurrentTag = gMemes.filter(function (meme) {
+        if (meme.tags.includes(tag))
+            return meme
+
+    }
+    )
+newImgs+= "<button class=\"arrow scroll-back\" onclick=\"onScroll(-1)\"></button>"
+
+    for (var i = 0;i < gCurrentTag.length; i++) {
+        newImgs += "<img class='img-option' id='img-" + (i + 1) + "' onclick='onSelectImg(this)'  src='imgs/" + gCurrentTag[i].id + ".jpg'>"
+        if (i === 2) break
+    }
+
+    newImgs+= "<button class=\"arrow scroll-forward\" onclick=\"onScroll(1)\"></button>"
+    currImgs.innerHTML = newImgs
 
 
 }
@@ -78,40 +132,60 @@ function onScroll(direction) {
     });
 }
 
-    function onApply(ev) {
-        if (gImage != 1) return //make sure there's an image in the canvas
-
-        const change = document.querySelector("#bottom")
-        change.innerHTML =
-            "<input type = \"text\" class=\"textType\"></input> <select id=\"chooseFont\">" +
-            
-            "<option value=\"IMPACT\">IMPACT</option>" +
-
-            "<option value=\"Arial\">Arial</option>" +
-            "<option value=\"Comic Sans MS\">Comic Sans</option>" +
-            "<option value=\"Times New Roman\">Times New Roman</option>"
-            +
-            "</select>" +
-            "<button onclick=\"onSubmit()\">Submit Text</button>"
-        
-
-    }
-
-    function onSubmit()
-    {
-        const input = document.querySelector(".textType")
-        const memeText = input.value
-
-        if(!memeText) return
-
-        drawText(memeText, gCanvas.width/2, gCanvas.height/2)
-    }
+function onApply(ev) {
+    if (gImage != 1) return //make sure there's an image in the canvas
+    const canvas = document.querySelector("canvas")
+    canvas.onclick= onPlace
 
 
+    const change = document.querySelector(".img-selectors")
+    change.innerHTML =
+        "<input type = \"text\" class=\"textType\" placeholder = \"type text here\"></input> <select id=\"chooseFont\">" +
 
+        "<option value=\"IMPACT\">IMPACT</option>" +
+
+        "<option value=\"Arial\">Arial</option>" +
+        "<option value=\"Comic Sans MS\">Comic Sans</option>" +
+        "<option value=\"Times New Roman\">Times New Roman</option>"
+        +
+        "</select>" +
+        "<button onclick=\"onFinished()\">Finished</button>"
+
+
+}
+
+function onFinished() {
+    const canvas = document.querySelector("canvas")
+    canvas.onclick= onPlace
+
+
+    const change = document.querySelector(".img-selectors")
+    change.innerHTML =
+        "<button class=\"download chooseImg\">Download</button>"
++
+"<button class =\"share\">Share</button>"
++
+        "<button class =\"facebook\">Share on Facebook</button>"
+}
+
+
+function onPlace(ev){
+   const {offsetX , offsetY} = ev
+    const input = document.querySelector(".textType")
+    const memeText = input.value
+
+     if (!memeText) return
+
+
+    drawText(memeText,offsetX,offsetY)
+
+
+   
+}
 function drawText(text, x, y) {
     gCtx.linewidth = 2
     gCtx.strokeStyle = 'Pink'
+    
 
     gCtx.font = '80px Impact'
     gCtx.textAlign = 'center'
